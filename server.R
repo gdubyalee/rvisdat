fa4eba8198d4c5cf0869e459c8c0ebe4f7509b98
George Lee <gl361@cam.ac.uk>
2016-06-23 17:37:34 +0100

    Added test data to pull down, started on server/ui

--- server.R
+++ server.R
@@ -1,3 +1,8 @@
+library(DriftR)
+library(InferCryptDrift)
+
+#Start up our server...
 library(shiny)
 shinyServer(function(input,output){
+  output$dataset<-readRDS(paste0('data/',input$searchStr))
 })
--- ui.R
+++ ui.R
@@ -1,3 +1,10 @@
 library(shiny)
 shinyUI(fluidPage(
+  sidebarPanel(
+    h3('Data visualisation ui'),
+    'Pull down data:',
+    textInput('searchStr','Dataset name'),
+    submitButton('Pull dataset'),
+  ),
+  uiOutput('dataset')
 ))
