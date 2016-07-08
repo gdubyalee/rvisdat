674787e0664c2b070f6ea0386c6c00a9827ca487
George Lee <gl361@cam.ac.uk>
2016-07-08 12:50:32 +0100

    wip

--- pcapp/pc.R
+++ pcapp/pc.R
@@ -29,74 +29,108 @@ handleUpload<-function(uploadedDataset){
   #Assume filename is [NAME].data for now
   saveName<-paste0(substr(uploadedDataset$name,1,nchar(uploadedDataset$name)-4),'rds')
   saveRDS(uploadedObj,paste0('data/',saveName))
-  print('Now to work out some params using the mcmc algorithm')
-  print(uploadedObj)
   saveRDS(getNeutralDriftParams(uploadedObj),paste0('cache/mcmc_',saveName))
 }
 
 processDataForPlots<-function(selectedDatasets){
-  if(is.null(selectedDatasets))return(NULL)
-  #Tend to get a nicer number of elements like this
-  timesForAnalyticFit<-seq(0,MOUSE_TIME-TIME_INTERVAL,TIME_INTERVAL)
-  #nBins<-16
-  mcmcParams<-list()
-  rawData<-list()
-  for(i in 1:length(selectedDatasets)){
-    mcmcParams[[i]]<-readRDS(paste0('cache/mcmc_',selectedDatasets[i]))
-    rawData[[i]]<-readRDS(paste0('data/',selectedDatasets[i]))
-    #nBins<-min(nBins,ncol(rawData[[i]]))
-  }
-
-  analyticList<-lapply(mcmcParams,function(obj)Crypt_drift_c(obj$lambda,obj$lambda,obj$N,timesForAnalyticFit,1,nBins))
-  #Add in a row for times
-  analyticList<-lapply(1:length(selectedDatasets),function(i){
-    rownames(analyticList[[i]])<-as.character(1:nBins)
-    ret<-rbind.data.frame(
-      times=timesForAnalyticFit,
-      analyticList[[i]]
+  #This feels rather hacky...
+  if(length(selectedDatasets)){
+    rawData<-cbind(
+      readRDS(paste0('data/',selectedDatasets[1])),
+      experiment=selectedDatasets[1],
+      proportion<-1:nBins
     )
-    ret<-rbind.data.frame(ret,experiment=selectedDatasets[i])
-    print(ret)
-    #names(ret)<-c('times',as.character(1:nBins))
-    ret
-  })
 
-  #names(analyticList)<-selectedDatasets
-  print(analyticList)
-  #frameToPlotAnalytic<-ldply(analyticList)
-  #frameToPlotAnalytic<-do.call('rbind',analyticList)
-  #print(frameToPlotAnalytic)
-  frameToPlotAnalytic<-analyticList[[1]]
+    analyticData<-cbind(
+      Crypt_drift_c(
+        )
+      time<-timesFor
+      )
+  }
   if(length(selectedDatasets)>1){
     for(i in 2:length(selectedDatasets)){
-      frameToPlotAnalytic<-cbind.data.frame(frameToPlotAnalytic,analyticList[[i]])
+      rawData<-rbind(
+        rawData,
+        cbind(
+          readRDS(paste0('data/',selectedDatasets[i])),
+          experiment=selectedDatasets[i],
+          proportion=1:nBins
+        )
+      )
     }
   }
-  #print(frameToPlotAnalytic)
-  frameToPlotRaw<-NULL
-  
-  ##Now squish so all have the right number of bins and put in a list
-  #frameToPlotRaw<-NULL
-  #for(i in 1:length(selectedDatasets)){
-  #  dat<-NULL
-  #  #Don't care about this yet
-  #  if(FALSE&&ncol(rawData[[i]])!=nBins){
-  #    tryCatch(someVariableWhichDoesNotExist,error=function(e)print('Haven\'t implemented comparison of multiple bin sized data yet!'))
-  #    #squish<-ncol(rawData[i])/nBins
-  #    #for(j in 1:nBins){
-  #    #  #Need to check indices are arranged the right way here
-  #    #  dat[j]<-colSums(rawData[j:(j+squish-1),])
-  #    #}
-  #  }else{
-  #    dat<-rawData[[i]]
-  #  }
-  #  dat[['experiment']]<-selectedDatasets[i]
-  #  dat[['times']]<-strtoi(substring(names(rawData[i]),2))
-  #  frameToPlotRaw<-rbind(frameToPlotRaw,dat)
-  #}
-
-  c(frameToPlotRaw,frameToPlotAnalytic)
+  if(length(selectedDatasets)){
+    print(rawData)
+    #print(analyticData)
+  }
 }
+
+#processDataForPlots<-function(selectedDatasets){
+#  if(is.null(selectedDatasets))return(NULL)
+#  #Tend to get a nicer number of elements like this
+#  timesForAnalyticFit<-seq(0,MOUSE_TIME-TIME_INTERVAL,TIME_INTERVAL)
+#  #nBins<-16
+#  mcmcParams<-list()
+#  rawData<-list()
+#  for(i in 1:length(selectedDatasets)){
+#    mcmcParams[[i]]<-readRDS(paste0('cache/mcmc_',selectedDatasets[i]))
+#    rawData[[i]]<-readRDS(paste0('data/',selectedDatasets[i]))
+#    #nBins<-min(nBins,ncol(rawData[[i]]))
+#  }
+#
+#  analyticList<-lapply(
+#    mcmcParams,
+#    function(obj)Crypt_drift_c(obj$lambda,obj$lambda,obj$N,timesForAnalyticFit,1,nBins)
+#  )
+#  #Add in a row for times
+#  analyticList<-lapply(1:length(selectedDatasets),function(i){
+#    rownames(analyticList[[i]])<-as.character(1:nBins)
+#    ret<-rbind.data.frame(
+#      times=timesForAnalyticFit,
+#      analyticList[[i]]
+#    )
+#    ret<-rbind.data.frame(ret,experiment=selectedDatasets[i])
+#    print(ret)
+#    #names(ret)<-c('times',as.character(1:nBins))
+#    ret
+#  })
+#
+#  #names(analyticList)<-selectedDatasets
+#  print(analyticList)
+#  #frameToPlotAnalytic<-ldply(analyticList)
+#  #frameToPlotAnalytic<-do.call('rbind',analyticList)
+#  #print(frameToPlotAnalytic)
+#  frameToPlotAnalytic<-analyticList[[1]]
+#  if(length(selectedDatasets)>1){
+#    for(i in 2:length(selectedDatasets)){
+#      frameToPlotAnalytic<-cbind.data.frame(frameToPlotAnalytic,analyticList[[i]])
+#    }
+#  }
+#  #print(frameToPlotAnalytic)
+#  frameToPlotRaw<-NULL
+#  
+#  ##Now squish so all have the right number of bins and put in a list
+#  #frameToPlotRaw<-NULL
+#  #for(i in 1:length(selectedDatasets)){
+#  #  dat<-NULL
+#  #  #Don't care about this yet
+#  #  if(FALSE&&ncol(rawData[[i]])!=nBins){
+#  #    tryCatch(someVariableWhichDoesNotExist,error=function(e)print('Haven\'t implemented comparison of multiple bin sized data yet!'))
+#  #    #squish<-ncol(rawData[i])/nBins
+#  #    #for(j in 1:nBins){
+#  #    #  #Need to check indices are arranged the right way here
+#  #    #  dat[j]<-colSums(rawData[j:(j+squish-1),])
+#  #    #}
+#  #  }else{
+#  #    dat<-rawData[[i]]
+#  #  }
+#  #  dat[['experiment']]<-selectedDatasets[i]
+#  #  dat[['times']]<-strtoi(substring(names(rawData[i]),2))
+#  #  frameToPlotRaw<-rbind(frameToPlotRaw,dat)
+#  #}
+#
+#  c(frameToPlotRaw,frameToPlotAnalytic)
+#}
 
 serve<-function(input,output){
 
@@ -111,7 +145,6 @@ serve<-function(input,output){
   })
   output$expectation<-renderPlot({
     renderedData<-processDataForPlots(input$datasets)
-    print(renderedData)
     plot(c(1,1,2,3,5,8,13,21))
   })
   observe({
