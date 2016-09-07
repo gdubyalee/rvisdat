fd472bc66f66332a2da8e43a5da06bdf9e920397
George Lee <gl361@cam.ac.uk>
2016-09-07 11:39:56 +0000

    OSX installation preparation

--- init.sh
+++ init.sh
@@ -5,6 +5,6 @@ ufw allow 15123
 ufw allow 20321
 
 #Create startup task (on osx)
-cp osxlaunchd.xml /System/Library/LaunchDaemons
+cp rvisdatstartup.plist /Library/LaunchAgents/rvisdatstartup.plist
 
 su george -c ./runapps.sh
