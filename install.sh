94dde1c90067cf2cb27a002406a8e32850ebdfb3
George Lee <gl361@cam.ac.uk>
2016-09-07 16:54:13 +0100

    Install script etc

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+#Install dependencies, something like
+apt-get install r-base-dev git
+cd /
+mkdir /srv
+cd srv
+#Pull down program...
+if [ ! -f rvisdat ]; then
+  git clone https://github.com/gdubyalee/rvisdat.git
+fi
+cd rvisdat
+./installdeps.Rscript
+
+
+#install DriftR, InferCryptDrift
+cd misc/CryptDrift1D
+R CMD INSTALL DriftR
+R CMD INSTALL InferCryptDrift
+cd ../..
+
+#modify firewall
+ufw allow 15123
+ufw allow 20321
+
+#Edit /etc/rc.local and add startup task
+sed -i -z 's/exit 0/\n\/srv\/rvisdat\/runapps.sh\nexit 0/'
+
