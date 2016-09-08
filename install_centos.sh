7634b0c964e202a6c34b3750cf993436565f8d56
George Lee <gl361@cam.ac.uk>
2016-09-08 17:51:43 +0100

    Tweaking install script

--- install_centos.sh
+++ install_centos.sh
@@ -1,22 +1,25 @@
 #!/bin/sh
 #Install dependencies, something like
+echo 'Yum and rpm things...'
+yum update -y
 yum groupinstall -y 'development tools'
 yum install -y wget which libcurl-devel openssl-devel git
 rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 yum install -y R
 
+echo 'Navigating to /srv...'
 cd /
 mkdir /srv
 cd srv
 
 #Pull down program...
 if [ ! -f rvisdat ]; then
+  echo 'Cloning repo...'
   git clone https://github.com/gdubyalee/rvisdat.git
 fi
 cd rvisdat
+echo 'Installing dependencies...'
 ./installdeps.Rscript
-
-
 #install DriftR, InferCryptDrift
 cd misc/CryptDrift1D
 R CMD INSTALL DriftR
@@ -24,11 +27,12 @@ R CMD INSTALL InferCryptDrift
 cd ../..
 
 #modify firewall
+echo 'Modifying iptables for ports 15123 and 20321...'
 iptables -A INPUT -m state --state NEW -p tcp --dport 15123 -j ACCEPT
 iptables -A INPUT -m state --state NEW -p tcp --dport 20321 -j ACCEPT
-/etc/init.d/iptables restart
 
 #Edit /etc/rc.local and add startup task
-echo '\n\n\/srv\/rvisdat\/runapps.sh\n\nexit 0\n' >>/etc/rc.d/rc.local
+printf '\n\n/srv/rvisdat/runapps.sh\n\nexit 0\n' >>/etc/rc.d/rc.local
 chmod +x /etc/rc.d/rc.local
 
+exit 0
