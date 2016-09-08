7f5eaca3edc11e909f47fcc9d44332a6a2b49966
George Lee <gl361@cam.ac.uk>
2016-09-08 18:23:45 +0100

    Installer tweaks

--- install_centos.sh
+++ install_centos.sh
@@ -27,12 +27,14 @@ R CMD INSTALL InferCryptDrift
 cd ../..
 
 #modify firewall
-echo 'Modifying iptables for ports 15123 and 20321...'
-iptables -A INPUT -m state --state NEW -p tcp --dport 15123 -j ACCEPT
-iptables -A INPUT -m state --state NEW -p tcp --dport 20321 -j ACCEPT
+#echo 'Modifying iptables for ports 15123 and 20321...'
+#iptables -A INPUT -m state --state NEW -p tcp --dport 15123 -j ACCEPT
+#iptables -A INPUT -m state --state NEW -p tcp --dport 20321 -j ACCEPT
 
 #Edit /etc/rc.local and add startup task
 printf '\n\n/srv/rvisdat/runapps.sh\n\nexit 0\n' >>/etc/rc.d/rc.local
 chmod +x /etc/rc.d/rc.local
+
+echo 'startup script added.  Please reboot!'
 
 exit 0
