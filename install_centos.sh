d2c6685e72986204320f8b53f51ac3caba5128b6
George Lee <gl361@cam.ac.uk>
2016-09-08 16:16:05 +0100

    Centos install sh

---
+++ install_centos.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+#Install dependencies, something like
+yum groupinstall -y 'development tools'
+yum install -y wget which libcurl-devel openssl-devel git
+rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum install -y R
+
+cd /
+mkdir /srv
+cd srv
+
+#Pull down program...
+if [ ! -f rvisdat ]; then
+  git clone https://github.com/gdubyalee/rvisdat.git
+fi
+cd rvisdat
+./installdeps.Rscript
+
+
+#install DriftR, InferCryptDrift
+cd misc/CryptDrift1D
+R CMD INSTALL DriftR
+R CMD INSTALL InferCryptDrift
+cd ../..
+
+#modify firewall
+ufw allow 15123
+ufw allow 20321
+
+#Edit /etc/rc.local and add startup task
+sed -i -z 's/exit 0/\n\/srv\/rvisdat\/runapps.sh\nexit 0/'
+
