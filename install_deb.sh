4522351fd3997713e2fc58cc52417a1a66f9192a
George Lee <gl361@cam.ac.uk>
2016-12-21 13:41:37 +0000

    Edit deb installer

--- install_deb.sh
+++ install_deb.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #Install dependencies, something like
-apt-get install gcc r-base-dev git
+apt-get -y install gcc r-base-dev git
 cd /
 mkdir /srv
 cd srv
@@ -18,9 +18,9 @@ R CMD INSTALL DriftR
 R CMD INSTALL InferCryptDrift
 cd ../..
 
-#modify firewall
-ufw allow 15123
-ufw allow 20321
+##modify firewall
+#ufw allow 15123
+#ufw allow 20321
 
 #Edit /etc/rc.local and add startup task
 sed -i 's/exit 0/\n\/srv\/rvisdat\/runapps.sh\nexit 0/' /etc/rc.local
